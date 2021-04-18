6761797cb95260fd4d9ea24b48aa17bc0e6caa94
bigoroberto <bigoroberto@gmail.com>
2021-04-18 15:57:42 +0200

    esercizio rifatto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,51 @@
-//REFERENCES
-
-var prezzoElementi = document.getElementById('price');
+//REFERENCES sono referenze che collegano la variante all'elemento index ( collegamenti all'index)
+var nameBurger = document.getElementById('name');
+var selectIngredient = document.getElementsByClassName('ingredient-checkbox');
+var discountCoupon = document.getElementById('coupon');
 var button = document.getElementById('button');
-var nameUser = document.getElementById('name');
-var ingredientiVari = document.getElementsByClassName('ingredient-checkbox');
+var totalPrice = document.getElementById('price');
 
-
-//SETTINGS
-
-var defaultPrezzo = 50;
-var coupons =  ['sconto2021','sconto-bool'];
-var sconto = 0.2;
-writeprice(defaultPrezzo, prezzoElementi);
+//SETTINGS sono le variabili globali non ancora collegate strettamente ad un elemento nell'index
+var arrCoupon = ['sconto2021', 'sconto-bool'];
+var discount = 0.3;
+var defaultPrice = 50;
+//adesso uso la funzione creata, con le mie variabili "reali" per scrivere il prezzo
+writePrice(defaultPrice, totalPrice);
 
 //EVENTS
-
-button.addEventListener('click', function (){
-  //controllo del nome
-  //verifico la presenza del nome
-  var nomeInserito = nameUser.value.trim();
-
-  if(nomeInserito.length === 0){
-    alert("Inserirsci il numer del tuo burger");
+button.addEventListener('click', function(){
+  var nomePanino = nameBurger.value.trim(); // quello che scrivo nel nome del panino diventa il suo value quindi richiamo quello
+  if(nomePanino.length === 0){   //mettiamo il (.length) perche il panino deve avere almeno un carattere all'interno di esso
+    alert("Nome inserito non valido!\nRiprova");
   }else{
-    //sommare gli elementi checkati
-    var prezzoIngrediente = 0;
-    //ciclo la lista degli input checkbox
-    for(var i = 0; i < ingredientiVari.length; i++){
-      var ingredient = ingredientiVari [i];
-      //se un checkbox è checked === true sonno il suo valore
-      if(ingredient.checked === true){
-        prezzoIngrediente += parseInt(ingredient.checked);
+    //creiamo un ciclo per gli ingredienti all'interno
+    // ci serve una variabile di partenza per il prezzo ( il panino non esiste)
+    var ingredientPrice = 0;
+    for(var i =0; i < selectIngredient.length; i++){
+      // creiamo la variabile per i singoli elementi
+      var singleElements = selectIngredient[i];
+      if(singleElements.checked === true){
+        //adesso sommiamo ogni singolo valore checkato alla variante iniziale 
+        ingredientPrice += parseInt(singleElements.value);
       }
     }
-    prezzoIngrediente.innerHTML= defaultPrezzo + prezzoIngrediente 
-    writeprice((defaultPrezzo + prezzoIngrediente), prezzoElementi);
   }
-  //verificare la presenza del coupon e nel caso applicare lo sconto
-  var prezzoTotale = defaultPrezzo + prezzoIngrediente;
 
-  // verifico la presenza del codice inserito dentro l'arrau dei coupons
-  if(coupons.includes('coupon.value')){
-    prezzoTotale -= prezzoTotale * sconto;
-    //oppure scriviamo ------prezzoTotale * (1-sconto);------- modalità da fico fatta vedere da stefano xD  
-  }else{
-    alert("Il coupon inserito non è scaduto oppure non è valido!")
+  // ho il prezzo di default e il prezzo degli ingredienti li sommo
+  var finalPrice = defaultPrice + ingredientPrice;
+  // facciamo lo sconto se viene utilizzato un coupon dell'array coupon
+  if(arrCoupon.includes(discountCoupon.value)){
+    finalPrice *= (1 - discount); // finalprice = finalprice * (1- discount)
   }
-  //stampare il prezzo totale
-  writeprice(prezzoTotale, prezzoElementi),
-})
+
+  //stampiamo
+  writePrice(finalPrice, totalPrice);
+
+
+});
+
 
 //FUNCTIONS
-
-//ricevere il valore da scrivere (value) e l'elemento dove essere scritto (target)
-function writeprice(value , target){
-  //assegnamo a targer un valore, al valore assegnamo il toFixed(2) per far comparire un esadecimale;
+function writePrice(value, target){
   target.innerHTML = value.toFixed(2);
 }
